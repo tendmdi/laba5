769b4bc56f60bd7b9c44b86b156b43d6020ff652
tendmdi <164333474+tendmdi@users.noreply.github.com>
2024-04-05 22:42:10 +0500

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,16 +1,23 @@
 import random
 
 def generatepas(length):
-    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    password = ''.join(random.choice(alphabet) for _ in range(length))
-    return password
+    for _ in range(length):
+        alphabet = random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        yield alphabet
 
 def invert_case(password):
     invertedpas = ''.join(map(lambda x: x.lower() if x.isupper() else x.upper(), password))
     return invertedpas
 
 length = 10
-password = generatepas(length)
+passwordg = generatepas(length)
+password = ''.join(passwordg)
+invertedpas = invert_case(password)
+
+print("Generated password:", password)
+print("Inverted password:", invertedpas)
+passwordg = generatepas(length)
+password = ''.join(passwordg)
 invertedpas = invert_case(password)
 
 print("Generated password:", password)
